bfa77ba9b77b510949c5ef37f5bb659f705ceaac
jjong0923 <90667780+jjong0923@users.noreply.github.com>
2025-11-18 15:52:46 +0900

    vite 기반 ts + tailwind 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello 간지톤</h1>
-    </div>
+    <>
+      <div>
+        <h1 className="text-3xl font-bold text-amber-600 underline">간지톤</h1>
+      </div>
+      <div className="h-10 w-12 bg-gray-400 text-white"></div>
+      <div className="flex bg-red-500 p-2 text-sm text-white">Test</div>
+      <h1 className="text-3xl font-bold text-amber-600 underline">텍스트</h1>
+    </>
   );
 }
 
