ee48d8f69d2702a03cf09f5c48a74167bf3b0374
jjong0923 <90667780+jjong0923@users.noreply.github.com>
2025-11-21 09:02:29 +0900

    Create Calendar.tsx

---
+++ src/Calendar/Calendar.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+function MyCalendar() {
+  // const [value, setValue] = useState(new Date());
+
+  return (
+    <div className="flex justify-center p-8">
+      <Calendar
+        // onChange={setValue}
+        value={value}
+        locale="en-US"
+        // calendarType="US"
+        nextLabel=">"
+        prevLabel="<"
+        next2Label={null}
+        prev2Label={null}
+        formatShortWeekday={(_, date) =>
+          ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][date.getDay()]
+        }
+      />
+    </div>
+  );
+}
+
+export default MyCalendar;
