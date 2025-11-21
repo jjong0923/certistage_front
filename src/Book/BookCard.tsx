444d5a354d811aa8bc7bfdc8cf2393c89fb26344
hayeoniill <lhy5441@naver.com>
2025-11-21 22:21:35 +0900

    교재 목록 1차 연동 certificatedId 1로 고정

--- src/Book/BookCard.tsx
+++ src/Book/BookCard.tsx
@@ -1,19 +1,28 @@
+import { PureComponent } from "react";
+
 type BookCardProps = {
-    image: string;
-    title: string,
-    publisher: string,
-    price: string;
+    image?: string | null;
+    title: string;
+    author: string;
+    price: number;
+    purchaseUrl: string;
 };
 
-function BookCard({ image, title, publisher, price }: BookCardProps) {
+function BookCard({ image, title, author, price }: BookCardProps) {
     return (
-        <div className="ml-[80px] mt-[30px] mb-[20px] mr-[25px]">
-            {image ? (<img src={image} className="w-[100px] h-[145px] object-cover rounded-[5px] mb-[10px]" />) :
-                (<div className="w-[100px] h-[145px] bg-[#D9D9D9] mb-[10px] shrink-0"></div>)}
-            <p>{title}</p>
-            <p className="text-xs text-gray-400"> {publisher} </p>
-            <p>{price}</p>
-        </div>
+        <a
+            href='{PureComponent}'
+            target="_blank"
+        >
+            <div className="mt-[30px] mb-[20px] mr-[25px]">
+                {image ? (<img src={image} className="w-[100px] h-[145px] object-cover rounded-[5px] mb-[10px]" />) :
+                    (<div className="w-[100px] h-[145px] bg-[#D9D9D9] mb-[10px] shrink-0"></div>)}
+                <p className="w-[250px]">{title}</p>
+                <p className="text-xs text-gray-400"> {author} </p>
+                <p>{price.toLocaleString()}원</p>
+            </div>
+        </a>
+
     );
 }
 
