16cdb10e810556fbf866f08c27c68ee620728444
jjong0923 <90667780+jjong0923@users.noreply.github.com>
2025-11-21 19:38:44 +0900

    Update Calendar.tsx

--- src/Calendar/Calendar.tsx
+++ src/Calendar/Calendar.tsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
 function MyCalendar() {
-  const [value, setValue] = useState(new Date());
+  // const [value, setValue] = useState(new Date());
 
   return (
     <div className="flex justify-center p-8">
       <Calendar
         // onChange={setValue}
-        value={value}
+        // value={value}
         locale="en-US"
         // calendarType="US"
         nextLabel=">"
