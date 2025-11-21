4f234284876b274ca3b8fea33407dd57ef923e0a
hayeoniill <lhy5441@naver.com>
2025-11-21 20:01:21 +0900

    헤더 로고 경로 수정

--- src/Book/BookReccomend.tsx
+++ src/Book/BookReccomend.tsx
@@ -2,7 +2,7 @@ import BookCard from "./BookCard";
 
 function BookReccomend() {
     return (
-        <div className="ml-[15%] mt-[50px] w-[1100px] min-w-[800px] rounded-[5px] shadow-[0px_4px_2.6px_0_rgba(0,0,0,0.25)]">
+        <div className="ml-[50px] mt-[30px] w-[1100px] min-w-[800px] rounded-[5px] shadow-[0px_4px_2.6px_0_rgba(0,0,0,0.25)]">
             <div className="shrink-0 pt-[20px] pl-[30px] font-semibold">
                 합격 스테이지로 가는 책
             </div>
